3ffd8615a56569693dae10da87fe170d00ba9d20
elmarkazumov <elmarkazumov20@gmail.com>
2023-02-05 11:54:22 +0500

    structure

---
+++ js/const.js
@@ -0,0 +1,9 @@
+const UIELEMENTS = {
+    inputsForm: document.querySelector(".main__block"),
+    taskInput: document.querySelector('.main__entry'),
+    taskButton: document.querySelector(".main__button"),
+    taskPriority: document.querySelector(".main__select"),
+    taskBlock: document.querySelector(".main__tasks"),
+    taskActual: document.querySelector(".main__actual"),
+    taskCompleted: document.querySelector(".main__completed"),
+};
